64d75efa3f32fb7720880ae9015faafc5eb9d37a
jojindragon <drgon5054@daum.net>
2025-03-26 15:22:08 +0900

    first commit

---
+++ src/main/java/com/example/demo/EpiMapApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpiMapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpiMapApplication.class, args);
+	}
+
+}
