b0a7115299aade7fc2a0677a3fd40ff6d0d6f306
Eunyoung-Choe <ceytown@student.hywoman.ac.kr>
2025-03-28 00:13:13 +0900

    Header, Navi 초안

--- src/main/java/data/controller/TestController.java
+++ src/main/java/data/controller/TestController.java
@@ -12,6 +12,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class TestController {
 	@RequestMapping("/")
 	public String hello() {
-		return "test";
+		return "layout/test";
 	}
 }
